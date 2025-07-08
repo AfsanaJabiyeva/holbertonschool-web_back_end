e6670ffc4bf44ad10c038c2397c039a6e0cb5c09
AfsanaJabiyeva <cebiyevaefsane737@gmail.com>
2025-07-08 14:27:31 +0400

    git push

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""insert new document"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    document = kwargs
+    result = mongo_collection.insert_one(document)
+    return result.inserted_id
+
